f11ef739b224bef90537730413624bd520d725b8
unknown <C:\Users\kalman\AppData\Roaming\The Bat!>
2012-08-10 15:52:49 +0300

    Depot scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '5d0fc591c44dc444f87dac536629a701f19c8ac2f5387e2b982ae232a0967ab229b51869a7aff176a282146d8fcbd7ef389047556b27d3cafba8600cf9c514ab'
