b69e30e70eb68745e7afaea4c430ced6858b03c6
Yury Bezhenar <yury_n@mail.ru>
2012-08-17 18:27:14 +0300

    current_item.quantity.to_i+1

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,11 +1,11 @@
 class Cart < ActiveRecord::Base
   # attr_accessible :title, :body
   has_many :line_items, :dependent => :destroy
-  has_many :quantity
+  #has_many :quantity
   def add_product(product_id)
     current_item = line_items.where(:product_id => product_id).first
     if current_item
-      current_item.quantity = current_item.quantity+1
+      current_item.quantity = current_item.quantity.to_i+1
     else
       current_item = line_items.build(:product_id => product_id)
     end
